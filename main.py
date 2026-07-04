9ea5caf3d91c9baceaa978dfa480c5244d3b6615
homelessAPI <minoloo546@gmail.com>
2026-07-04 11:10:45 -0400

    Version 1.0 with a completed loop and error handling

---
+++ main.py
@@ -0,0 +1,44 @@
+import requests
+from tabulate import tabulate
+
+
+
+def github_activity(username):
+    url = f"https://api.github.com/users/{username}"
+    events_url = f"https://api.github.com/users/{username}/events"
+    repos_url = f"https://api.github.com/users/{username}/repos"
+
+    try:
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            events_data = requests.get(events_url).json()
+
+            if events_data == []:
+                print(f"No recent activity found for user '{username}'.")
+                
+            else:
+                rows = []
+                for i in events_data:
+                    rows.append([
+                    i["type"],
+                    i["repo"]["name"],
+                    i["repo"]["url"],
+                    i["created_at"],
+                    i["public"]
+                ])
+
+                header = ["Event Type", "Repository", "Repository_URL", "Created At", "Public"]
+                print(tabulate(rows, headers=header, tablefmt="grid"))
+
+        else:
+            print(f"Failed to retrieve data for user. Status code: {response.status_code}")
+    except Exception as e:
+        print(f"An error occurred while making the API request: {e}")
+
+if __name__ == "__main__":
+    while True:
+        username = input("Enter a GitHub username (or 'exit' to quit): ")
+        if username.lower() == 'exit':
+            break
+        github_activity(username)
