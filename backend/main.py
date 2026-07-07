ab776e03eea9eec62949177909cb2d686ceb20b4
homelessAPI <minoloo546@gmail.com>
2026-07-07 07:26:06 -0400

    redeploy

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5174",
-        "https://cain.app"
+        "https://cain-ny53l4byc-spade2.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
