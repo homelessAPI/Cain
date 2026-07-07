fa28386b962873051c0e0e2b81dc6464259a89b9
homelessAPI <minoloo546@gmail.com>
2026-07-07 07:41:44 -0400

    redeploy 2

--- frontend/Cain/src/App.jsx
+++ frontend/Cain/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
       username: username
     }
 
-    const response = await fetch("http://127.0.0.1:5000/",
+    const response = await fetch("https://cain-h3k7.onrender.com/",
       {
         method: 'POST',
         headers: {
