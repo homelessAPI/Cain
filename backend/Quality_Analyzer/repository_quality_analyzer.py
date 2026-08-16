6877752dbe679feea868275933b1101c7ad4613a
homelessAPI <minoloo546@gmail.com>
2026-08-16 16:24:20 -0400

    Activity analysis

--- backend/Quality_Analyzer/repository_quality_analyzer.py
+++ backend/Quality_Analyzer/repository_quality_analyzer.py
@@ -5,22 +5,28 @@ class QuslityAnanlysis:
     def __init__(self, username):
         self.username = username
 
+        self.Fetchdata = FetchData(self.username)
+        self.repos = self.Fetchdata.repos()
+
         self.Documentation_score = 0
         self.Engineering_score = 0
         self.Repo_hygiene_score = 0
         self.DevOps_score = 0
+
+        self.total_repo_score = 100
+        self.percentage_repo_Score = 0
+        self.assigned_letter
+
         self.Score_dict = {}
 
     def documentation(self):
-        Fetchdata = FetchData(self.username)
-        repos = Fetchdata.repos()
-        repos_analyser = RepoAnalyser(repos)
+        repos_analyser = RepoAnalyser(self.repos)
 
-        for i in repos:
+        for i in self.repos:
             repo_documentation_data = repos_analyser.repo_content_review(self.username, i["Repo_name"])
 
             if repo_documentation_data["README"] == True:
-                self.Documentation_score += 10
+                self.Documentation_score += 30
 
             if repo_documentation_data[".gitignore"] == True:
                 self.Repo_hygiene_score += 10
@@ -29,10 +35,10 @@ class QuslityAnanlysis:
                 self.Repo_hygiene_score += 10
 
             if repo_documentation_data["Tests"] == True:
-                self.Engineering_score += 10
+                self.Engineering_score += 30
 
             if repo_documentation_data["Dockerfile"] == True:
-                self.DevOps_score += 10
+                self.DevOps_score += 20
 
             self.Score_dict[i["Repo_name"]] = {"documentation": self.Documentation_score,
                                                "Engineering_score": self.Engineering_score,
@@ -46,5 +52,27 @@ class QuslityAnanlysis:
 
         return self.Score_dict
 
-    def Overall_Score():
-        pass
+    def Overall_Score(self):
+        total_repos = len(self.Score_dict)
+        total_possible_score = self.total_repo_score * total_repos
+
+        repos_score = 0
+
+        for i in self.Score_dict:
+            repos_score += sum(i.values())
+
+        self.percentage_repo_Score = (repos_score/total_possible_score) * 100
+
+        if 90 <= self.percentage_repo_Score <= 100:
+            self.assigned_letter = "A"
+
+        if 80 <= self.percentage_repo_Score <= 89:
+                    self.assigned_letter = "B"
+
+        if 70 <= self.percentage_repo_Score <= 79:
+                    self.assigned_letter = "C"
+
+        if 60 <= self.percentage_repo_Score <= 69:
+                    self.assigned_letter = "D"
+
+        return self.percentage_repo_Score
